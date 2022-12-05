bc8d7b5bbcae5cabbcd4af60a844fc3dce0232bf
Daniel3178 <89932502+Daniel3178@users.noreply.github.com>
2022-12-05 23:17:17 +0100

    Started assembling the methods, AccountCreator class is 90% done!

--- ATMSimulator/AccountCreator.cs
+++ ATMSimulator/AccountCreator.cs
@@ -9,31 +9,89 @@ namespace ATMSimulator
 {
     internal class AccountCreator
     {
+        private static bool AccountCreatorOptionIsActive = false;
+        private static bool AccountCreatorIsActive = false;
         public static List<uint> IDNumberList = new List<uint>();
         public static List<uint> CardNumberList = new List<uint>();
+        private static Account? activeAccount;
+        private enum Options { Profile = 1, New, Back }
         public static void Run()
         {
-            for (uint i = 0; i < 1; i++)
+            AccountCreatorIsActive = true;
+            while (AccountCreatorIsActive)
             {
+                Menu.ShowTheSummary();
+                CreateAccount();
+                OptionsManager();
 
-                Account newAccount;
-                Console.WriteLine("please enter your full name:");
-                string[] temp = GetUserFullName();
-                Console.WriteLine("please enter your IdNumber:");
-                uint id = GetUserIDNumber(Console.ReadLine());
-                Console.WriteLine("please choose a password");
-                uint password = GetUserPassword();
-                Console.WriteLine("your cardnumber is : ");
-                uint cardNum = CardNumGenerator();
-                Console.WriteLine(cardNum);
-
-                newAccount = new Account(id, temp[0], temp[1], cardNum, password);
-                AccountManager.AddNewAccountToDictionary(newAccount, cardNum);
             }
             AccountManager.ListAllTheAccounts();
             DataHandler.WriteToDatabase();
         }
 
+        private static void CreateAccount()
+        {
+            Account newAccount;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("\t"+"please enter your full name: ");
+            string[] temp = GetUserFullName();
+            Console.Write("\t" + "please enter your IdNumber: ");
+            uint id = GetUserIDNumber(Console.ReadLine());
+            Console.Write("\t" + "please choose a 6-digit PIN kod: ");
+            uint password = GetUserPassword();
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\n \t Congratulation!! Your account has been created!" +
+                "\n \t Your cardnumber is: ");
+            uint cardNum = CardNumGenerator();
+            Console.WriteLine(cardNum);
+            Console.ResetColor();
+
+            newAccount = new Account(id, temp[0], temp[1], cardNum, password);
+            AccountManager.AddNewAccountToDictionary(newAccount, cardNum);
+            activeAccount = newAccount;
+
+        }
+        private static void ShowTheProfile()
+        {
+            if(activeAccount != null)
+            {
+                Console.WriteLine(activeAccount.ToString());
+            }
+        }
+        public static void OptionsManager()
+        {
+            AccountCreatorOptionIsActive = true;
+            while (AccountCreatorOptionIsActive)
+            {
+
+                Console.WriteLine("\t" + "[PRESS 1] Show the account profile");
+                Console.WriteLine("\t" + "[PRESS 2] Create a new account");
+
+                Console.WriteLine("\t" + "[PRESS 3] Get back to Simulator");    
+
+                int temp = 0;
+                while (temp > 2 || temp < 1)
+                {
+                    temp = Menu.GetTheUserChoice(Console.ReadLine());
+                }
+                switch (temp)
+                {
+                    case (int)Options.Profile:
+                        ShowTheProfile();
+                        break;
+
+                    case (int)Options.New:
+                        AccountCreatorOptionIsActive=false;
+                        break;
+                    case (int)Options.Back:
+                        AccountCreatorOptionIsActive = false;
+                        AccountCreatorIsActive = false;
+                        break;
+
+                }
+            }
+        }
         public static bool IsUniqueID(uint idToCheck)
         {
             if (IDNumberList.Contains(idToCheck))
